105b21b8aaefa315025625d7e9b08e1fb43b96a6
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-05-12 22:02:42 -0400

    2 - Variáveis e Operadores | 3 - Operadores Lógicos - OK, funcionando

--- js/principal.js
+++ js/principal.js
@@ -9,7 +9,24 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
-
 var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+
+var pesoValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido";
+    pesoValido = false;
+}
+
+if(altura <= 0 || altura >= 3) {
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida";
+    alturaValida = false;
+}
+
+if(pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
