1830e8ae390ed7c113ebfaa0e75dba255b7458a9
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-05-13 10:07:48 -0400

    4 - Eventos, Formulários e Criando Elementos | 1 - Escutando eventos - OK, funcionando

--- js/principal.js
+++ js/principal.js
@@ -1,30 +1,48 @@
-//var titulo = document.querySelector("#titulo");
-//titulo.textContent = "Aparecida Nutricionista";
+var titulo = document.querySelector("#titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
 for(var i = 0; i < pacientes.length; i++) {
     var peso = pacientes[i].querySelector(".info-peso").textContent;
     var altura = pacientes[i].querySelector(".info-altura").textContent;
-    var imc = pacientes[i].querySelector(".info-imc").textContent;
+    var imc = pacientes[i].querySelector(".info-imc");
 
     var pesoValido = true;
     var alturaValida = true;
 
     if(peso <= 0 || peso >= 1000) {
-        imc = "Peso inválido";
+        imc.textContent = "Peso inválido";
         pesoValido = false;
         pacientes[i].classList.add("paciente-invalido");
     }
 
     if(altura <= 0 || altura >= 3) {
-        imc = "Altura inválida";
+        imc.textContent = "Altura inválida";
         alturaValida = false;
         pacientes[i].classList.add("paciente-invalido");
     }
 
     if(pesoValido && alturaValida){
         var valorImc = peso / (altura * altura);
-        imc = valorImc.toFixed(2);
+        imc.textContent = valorImc.toFixed(2);
     }
 }
+
+//Função Nomeada
+//titulo.addEventListener("click", mostraMensagem);
+
+//function mostraMensagem() {
+//    console.log("Olá, eu fui clicado");
+//}
+
+//Função Anônima
+//titulo.addEventListener("click", function() {
+//    console.log("Olá, eu fui clicado");
+//});
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Olá, eu fui clicado");
+});
