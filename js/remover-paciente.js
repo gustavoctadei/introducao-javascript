abd858e78e08725fc3cfb89129ec395584e21474
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-05-22 12:00:40 -0400

    7 - Remoção, Delegação e Animação - OK, funcionando

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,12 +3,18 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.remove();
+    var trPaciente = event.target.parentNode;
+    trPaciente.classList.add("fadeOut");
+    setTimeout(function(){ //espera 500 milissegundos e executa a função
+        trPaciente.remove();
+    }, 500);
+
+    
 /*     var alvoEvento = event.target;
     var paiAlvo = alvoEvento.parentNode;
     
     paiAlvo.remove(); */
-})
+});
 
 /* pacientes.forEach(function(paciente){
     paciente.addEventListener("dblclick", function(){
