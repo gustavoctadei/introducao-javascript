0dd95cc55a0fdba247397913c32380a62f9db2df
Gustavo <gustavo.tadei@hotmail.com>
2020-06-03 17:42:47 -0400

    9 - Ajax Buscando pacientes com AJAX | 3 - Capturando erros de requisicao - OK, funcionando

--- js/importar-pacientes.js
+++ js/importar-pacientes.js
@@ -8,15 +8,26 @@ botaoImportar.addEventListener("click", function(){
 
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    //xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes11111");
     xhr.addEventListener("load", function(){
         //console.log(xhr.responseText);
-        var resposta = xhr.responseText;
-        //console.log(typeof resposta);
-        var pacientes = JSON.parse(resposta);
-        //console.log(pacientes);
-        pacientes.forEach( function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            //console.log(typeof resposta);
+            var pacientes = JSON.parse(resposta);
+            //console.log(pacientes);
+            pacientes.forEach( function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+        }
+        else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
     });
     xhr.send();
 });
