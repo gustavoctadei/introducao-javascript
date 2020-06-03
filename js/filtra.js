566e8d18bf6a6c0a097e85c54861d00e35729044
Gustavo <gustavo.tadei@hotmail.com>
2020-06-03 15:04:11 -0400

    8 - Filtrando uma tabela | 2 - Implementando a Logica de filtragem - OK, funcionando

--- js/filtra.js
+++ js/filtra.js
@@ -3,14 +3,30 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
     //console.log(campoFiltro.value);
-    console.log(this.value);
+    //console.log(this.value);
 
     var pacientes = document.querySelectorAll(".paciente");
 
-    for(var i = 0; pacientes.length; i++){
-        var paciente = pacientes[i];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
+    if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            //console.log(nome);
+    
+            if(nome != this.value){
+                paciente.classList.add("invisivel");
+            }
+            else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }
+    else{
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
     }
 
 });
